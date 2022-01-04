c313d7bb27c5f8675d9451225c456cbcde709a3a
Carolina Gonzalez <carolina.nicole.gonzalez@gmail.com>
2022-01-04 15:29:26 -0700

    asset patch event should include type

--- src/components/AprimoWidget.tsx
+++ src/components/AprimoWidget.tsx
@@ -52,6 +52,7 @@ const AprimoWidget = (props: Props) => {
 
   const setAsset = (asset: Record<string, any>) => {
     asset._key = value && value._key ? value._key : nanoid()
+    asset._type = type
     onChange(PatchEvent.from(asset ? set(asset) : unset()))
   }
 
